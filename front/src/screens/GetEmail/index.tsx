611b14a586caf72fc63a90d2591b041f10537d7e
adriellyb <adrielly0195@gmail.com>
2022-02-21 18:06:52 -0300

    feature: criando tela GetEmail e estilizando

--- front/src/screens/GetEmail/index.tsx
+++ front/src/screens/GetEmail/index.tsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
 
-export default function GetEmail() {
+/** */
+import { styles } from './styles';
+
+export default function GetEmail({ navigation, route }: any) {
+
+    const [text, setText] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const [loaded] = useFonts({
+        Light: require('../../../assets/fonts/RobotoMono-Light.ttf'),
+        Regular: require('../../../assets/fonts/RobotoMono-Regular.ttf'),
+        Medium: require('../../../assets/fonts/RobotoMono-Medium.ttf'),
+        SemiBold: require('../../../assets/fonts/RobotoMono-SemiBold.ttf'),
+        Bold: require('../../../assets/fonts/RobotoMono-Bold.ttf'),
+    });
+
+    useEffect(() => {
+        if (route.params?.name) {
+            setAuthor(route.params?.name);
+        }
+    }, [route.params?.name]);
+
+
+    const goToWriteMessage = () => {
+        {
+            navigation.navigate({
+                name: "WriteMessage",
+                params: {
+                    data: {
+                        author: author,
+                        email: text
+                    }
+                },
+                merge: true,
+            })
+        }
+    }
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
-        <></>
+        <ImageBackground
+            source={require("../../../assets/background2.png")}
+            style={styles.imageView}
+        >
+            <Shadow offset={[-6, 6]} startColor={'#000'} finalColor={"#000"} distance={3}>
+                <View style={styles.stepView}>
+                    <Text style={[styles.stepText, { fontFamily: "Bold" }]}>1</Text>
+                </View>
+            </Shadow>
+
+            <View style={styles.viewContent}>
+                <Shadow offset={[-6, 6]} startColor={'#000'} finalColor={"#000"} distance={3}>
+                    <View style={styles.boxContent}>
+                        <Text style={[styles.labelTitle, { fontFamily: "Bold" }]}>Pra quem você quer mandar?</Text>
+                        <TextInput
+                            value={text}
+                            placeholder="exemplo@gmail.com"
+                            onChangeText={(text: any) => setText(text)}
+                            style={styles.input}
+                        />
+                    </View>
+                </Shadow>
+
+                <Shadow offset={[-6, 6]} startColor={'#000'} finalColor={"#000"} distance={3}>
+                    <TouchableOpacity
+                        style={styles.buttonView}
+                        activeOpacity={.8}
+                        onPress={() => { goToWriteMessage() }}
+                    >
+                        <Text style={[styles.buttonText, { fontFamily: "Bold" }]}>Seguir</Text>
+                        <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+                    </TouchableOpacity>
+                </Shadow>
+            </View>
+        </ImageBackground>
     )
 }
---
+++ front/src/screens/GetEmail/styles.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    imageView: {
+        flex: 1,
+        flexDirection: "column",
+        paddingHorizontal: "10%",
+        paddingVertical: "20%"
+    },
+    stepView: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#FFF",
+        borderRadius: 500,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    stepText: {
+        fontSize: 18,
+    },
+    viewContent: {
+        flex: 1, 
+        alignItems: "center", 
+        justifyContent: "space-evenly"
+    },
+    boxContent: {
+        backgroundColor: "#FFF",
+        height: 120,
+        width: 280,
+        borderWidth: 2
+    },
+    labelTitle: {
+        fontSize: 18,
+
+        paddingHorizontal: "5%",
+        paddingTop: 10
+    },
+    input: {
+        borderBottomWidth: 1,
+        width: "90%",
+        alignSelf: "center",
+        fontSize: 16,
+
+        paddingTop: 15
+    },
+    buttonView: {
+        height: 50,
+        width: "auto",
+        backgroundColor: "#FE9FE6",
+        borderWidth: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+
+        paddingHorizontal: "10%"
+    },
+    buttonText: {
+        fontSize: 18,
+        paddingRight: 10
+    }
+})
