22fa3e3a839222ded59a4f56626e70e55838eff5
adriellyb <adrielly0195@gmail.com>
2022-02-23 15:40:19 -0300

    feature: criando tela Success Screen e estilizando

--- front/src/screens/SuccessScreen/index.tsx
+++ front/src/screens/SuccessScreen/index.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Image, ImageBackground, Text, TouchableOpacity } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
 
-export default function SuccessScreen() {
+/** */
+import { styles } from './styles';
+
+export default function SuccessScreen({ navigation }:any) {
+
+    const [loaded] = useFonts({
+        Medium: require('../../../assets/fonts/RobotoMono-Medium.ttf'),
+        SemiBold: require('../../../assets/fonts/RobotoMono-SemiBold.ttf'),
+        Bold: require('../../../assets/fonts/RobotoMono-Bold.ttf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
-        <></>
+        <ImageBackground
+            source={require('../../../assets/background.png')}
+            style={styles.imageView}
+        >
+            <Image
+                source={require('../../../assets/happyface.png')}
+                style={{width: 130 , height: 130 }}
+            />
+
+            <Text style={[styles.textView, {fontFamily: "Medium"}]}>Mensagem enviada com sucesso!</Text>
+
+            <Shadow offset={[-6, 6]} startColor={'#000'} finalColor={"#000"} distance={3}>
+                <TouchableOpacity
+                    style={styles.buttonView}
+                    activeOpacity={.8}
+                    onPress={() => { navigation.navigate("Home") }}
+                >
+                    <Text style={[styles.buttonText, {fontFamily: "Bold"}]}>Início</Text>
+                    <Feather name='home' size={24} color={"#000"} />
+                </TouchableOpacity>
+            </Shadow>
+
+            <Image
+                source={require('../../../assets/homeicon.png')}
+                style={{ width: 50, height: 50, alignSelf: "center" }}
+            />
+        </ImageBackground>
     )
 }
